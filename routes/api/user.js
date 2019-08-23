1cfee949181a67d810f7ab327aa11c4db3b73cb6
rajan <rajangreedge@gmail.com>
2019-08-23 20:41:58 +0530

    check if user exist api/user (registration/route)

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const router =  express.Router();
+const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 
-const router =  express.Router();
+const User = require('../../models/User');
+
 
 /**
  * @route   POST api/users
@@ -12,12 +15,45 @@ router.post('/',[
   check('name', 'Name is required').not().isEmpty(),
   check('email', 'Please enter a password with 6 or more characters').isEmail(),
   check('password', 'Please enter a password with 6 or mre characters').isLength({min: 6})
-], (req,res)=>{
+], async (req,res)=>{
   //console.log(req.body);
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res.status(400).json({error: errors.array()}); //errors formate {error: [{location, param , msg}]}
   }
+
+  const {name, email, password} = req.body;
+  
+
+  try {
+    //see if user exists
+    let user = await User.findOne({email});
+    if(user) {
+      //bad req
+      res.status(400).json({errors: [{msg: 'User already exists'}]});
+    }
+    //Get users gravatar
+    const awatar = gravatar.url(email, {
+      s: '200', //size
+      r: 'pg', //rating
+      d: 'mm' //..
+    })
+
+    user = new User({
+      name,
+      email,
+      awatar,
+      password
+    });
+    
+    //Encrypt password
+  }catch(err){
+    console.log(err.message);
+    res.status.send('Server error');
+  }
+
+
+  //Return
   res.send('User route')
 })
 
