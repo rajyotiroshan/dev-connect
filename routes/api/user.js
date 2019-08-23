c8c74190f5497ac87dd5c887de89364d6fdaf4e8
rajan <rajangreedge@gmail.com>
2019-08-23 20:26:53 +0530

    express-validator middleware on api/user route

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const router =  express.Router();
 
@@ -7,8 +8,16 @@ const router =  express.Router();
  * @desc    Register route
  * @access  Public
  */
-router.post('/',(req,res)=>{
-  console.log(req.body);
+router.post('/',[
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please enter a password with 6 or more characters').isEmail(),
+  check('password', 'Please enter a password with 6 or mre characters').isLength({min: 6})
+], (req,res)=>{
+  //console.log(req.body);
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({error: errors.array()}); //errors formate {error: [{location, param , msg}]}
+  }
   res.send('User route')
 })
 
