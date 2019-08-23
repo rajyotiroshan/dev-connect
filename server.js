ab3a8903973e0d61da30fdbbc7fe9fa200008b8d
rajan <rajangreedge@gmail.com>
2019-08-23 19:29:53 +0530

    Basic routes module setup

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ connectDB();
 const PORT = process.env.PORT||5000; //look for env var port on deploying
 
 app.get('/',(req,res)=>{
-  res.send("APO running");
+  res.send("API running");
 })
+
+//Define routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+
 app.listen(PORT, ()=>{
   console.log("Server started on port...", PORT);
 })
