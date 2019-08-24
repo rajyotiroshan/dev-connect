b7bf823b5938c4d7ebeb8298917e4a63278ff253
rajan <rajangreedge@gmail.com>
2019-08-24 20:26:17 +0530

    access authenticated user from inside api/auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,14 +2,20 @@ const express = require('express');
 const router =  express.Router();
 
 const auth = require('../../middleware/auth');
-
+const User = require('../../models/User');
 /**
  * @route   GET api/auth
  * @desc    Test route
  * @access  Public
  */
-router.get('/',auth,  (req, res) => {
-  res.send('Auth route');
+router.get('/',auth, async (req, res) => {
+  try {
+    //access the user 
+    const user = await User.findById(req.user.id).select('-password');//do not select the password.
+    res.status(200).json(user);
+  }catch(err) {
+    res.status(500).send({error: 'bad request'});//server error
+  }
 });
 
 module.exports = router;
