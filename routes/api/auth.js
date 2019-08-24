dec1766c978f4174ba91164932e425e537e23d20
rajan <rajangreedge@gmail.com>
2019-08-24 20:14:06 +0530

    included auth middleware in /api/auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
 const router =  express.Router();
+
+const auth = require('../../middleware/auth');
 
 /**
  * @route   GET api/auth
  * @desc    Test route
  * @access  Public
  */
-router.get('/', (req, res) => {
+router.get('/',auth,  (req, res) => {
   res.send('Auth route');
 });
 
