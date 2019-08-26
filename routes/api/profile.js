08c067bbb8a365a46c67fc04cfb2e900d33b4e86
rajan <rajangreedge@gmail.com>
2019-08-26 11:00:11 +0530

    api/profile/experience route for add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -173,5 +173,61 @@ router.get('/me',auth,
       }
     }
    )
+     /**
+   * @route PUT api/profile/experience
+   * @desc Add Profile expreience
+   * @access Private
+   */
+
+   router.put(
+     '/experience',
+     [
+       auth,
+       [
+         check('title', 'Title is required')
+           .not()
+           .isEmpty(),
+         check('from', 'From data is required')
+           .not()
+           .isEmpty()
+       ]
+     ],
+     async (req, res) => {
+         const errors = validationResult(req);
+         if(!errors.isEmpty()){
+           return res.status(400).json({ erros: errors.array()});
+         }
+         const {
+           title,
+           company,
+           location,
+           from,
+           to,
+           current,
+           description
+         } = req.body;
+
+         const newExp = {
+           title,
+           company,
+           location,
+           from,
+           to,
+           current,
+           description
+         } 
+          try{
+            let profile = await Profile.findOne({ user: req.user.id});
+            //console.log(profile); working
+            profile.experience.unshift(newExp);
+
+            await profile.save();
+            res.json(profile);
+          }
+         catch (error) {
+         console.log(error.message);
+         res.status(500).send('Server error');
+       }
+      });
 
 module.exports = router;
