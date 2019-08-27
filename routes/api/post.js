1ceab3fa54a1b7f2332c426b672b60bcb76494fd
rajan <rajangreedge@gmail.com>
2019-08-27 14:09:47 +0530

    GET api/post route for lall post

--- routes/api/post.js
+++ routes/api/post.js
@@ -43,4 +43,22 @@ router.post('/',[
 
 });
 
+/**
+ * @route   GET api/posts
+ * @desc    Get all posts
+ * @access  Private
+ */
+
+ router.get('/', auth, async (req,res)=>{
+   try {
+     
+    const posts = await Post.find().sort( { date: 1} );
+    res.status(200).json(posts);
+
+   } catch (error) {
+     console.log(error.message);
+     res.status(500).send('Server error');
+   }
+ })
+
 module.exports = router;
