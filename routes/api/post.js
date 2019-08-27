d518b0950a85937e25566626a8089ce7ebe4d22e
rajan <rajangreedge@gmail.com>
2019-08-27 15:34:41 +0530

    DELETE api/post/:post_id

--- routes/api/post.js
+++ routes/api/post.js
@@ -88,4 +88,39 @@ router.post('/',[
    }
  })
 
+  /**
+ * @route   DELETE api/posts/:id
+ * @desc    Delete a post by id
+ * @access  Private
+ */
+
+ router.delete('/:post_id', auth, async (req,res)=>{
+
+   try {
+    //
+    const post = await Post.findById(req.params.post_id);
+
+    console.log(post);
+    if(!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    //make sure user owned the post
+    if(post.user.toString() !== req.user.id){
+      return res.status(401).json({ msg:'USer not authorised' })
+    }
+
+    await post.remove();
+    res.status(200).json({ msg: 'Post removed'});
+
+   } catch (error) {
+     console.log(error.message);
+     
+    if (!error.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+     res.status(500).send('Server error');
+   }
+ })
+
 module.exports = router;
