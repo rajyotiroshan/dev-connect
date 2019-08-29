fb4f8813e3cfe046320a32729021833a4c105848
rajan <rajangreedge@gmail.com>
2019-08-29 15:05:54 +0530

    registration form with useState

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className='navbar bg-dark'>
       <h1>
         <Link to='/'>
-          <i class='fas fa-code'></i> DevConnector
+          <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
       <ul>
