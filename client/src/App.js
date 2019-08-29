bf558ae667d43d7ccdcf664cc5311c30f38e67db
rajan <rajangreedge@gmail.com>
2019-08-29 14:09:33 +0530

    addes routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { Fragment } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing'
 import './App.css';
 
 const  App = ()=>{
   return (
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path="/" component= { Landing } />
+      </Fragment>   
+    </Router>
+
   )
 }
 
