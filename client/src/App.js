ddb2e2b98c562b1cbf46e6808a714fbf0c74039e
rajan <rajangreedge@gmail.com>
2019-08-29 14:02:10 +0530

    Navbar, Landing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Fragment } from 'react';
+import Navbar from './components/layouts/Navbar';
+import Landing from './components/layouts/Landing'
 import './App.css';
+
 const  App = ()=>{
   return (
     <Fragment>
-      <h1>App</h1>
+      <Navbar/>
+      <Landing/>
     </Fragment>
   )
 }
