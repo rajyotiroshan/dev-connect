cc989b608ce47eafcff6184194e06272e39281ed
rajan <rajangreedge@gmail.com>
2019-09-02 12:19:04 +0530

    USER_LOADED, AUTH_ERROR REDUCERS CASE

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,32 +1,46 @@
-import {REGISTER_SUCCESS, REGISTER_FAIL} from '../actions/Types';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR
+} from '../actions/Types';
+import { stat } from 'fs';
 
 const initialState = {
   token: localStorage.getItem('token'),
   isAuthenticated: null,
   loading: true,
   user: null
-}
+};
 
-export default function(state= initialState, action){
-  const { type, payload} = action;
-  switch(type){
+export default function(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_LOADED:
+      return {
+        ...state,
+        isAuthenticated: true,
+        loading: false,
+        user: payload
+      };
     case REGISTER_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
-        ...state, 
-        ...payload, 
-        isAuthenticated: true, 
+        ...state,
+        ...payload,
+        isAuthenticated: true,
         loading: false
-      }
+      };
     case REGISTER_FAIL:
+    case AUTH_ERROR:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
         isAuthenticated: false,
         loading: false
-      }
+      };
     default:
-      return state
+      return state;
   }
 }
