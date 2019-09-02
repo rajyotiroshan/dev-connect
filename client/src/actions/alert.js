792901d029f6244434806d0161fb9950db2ef394
rajan <rajangreedge@gmail.com>
2019-09-02 10:03:20 +0530

    dispatch remove alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,13 @@
 import uuid from 'uuid';
 import {SET_ALERT, REMOVE_ALERT} from './Types';
 
-export const setAlert = (msg, alertType)=> dispatch => {
+export const setAlert = (msg, alertType, timeout =5000)=> dispatch => {
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id}
   });
+
+  //remove alert after 5 sec
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload:id}), (timeout));
 }
