3e8edf4584c93bf374cf395fcf3b2fdc4ff9f3f4
rajan <rajangreedge@gmail.com>
2019-09-02 10:02:46 +0530

    implement alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
-import React,{ Fragment, useState} from 'react'
+import React,{ Fragment, useState} from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {setAlert} from '../../actions/alert';
+import PropTypes from 'prop-types'
+
 //import axios from 'axios';
 
-const Register = () => {
+const Register = ({setAlert}) => {
   const [formData, setFormData] = useState(
     {
       name: '',
@@ -22,7 +26,8 @@ const Register = () => {
     //
     e.preventDefault();
     if(password !== password2) {
-      console.log(" Password do not match. ");
+      //console.log(" Password do not match. ");
+      setAlert('Password did not match', 'danger');
     }else {
 /*  WILL IMPLEMENT THIS CODE INSIDE REDUX ACTION   
  const newUser = {
@@ -101,4 +106,8 @@ const Register = () => {
   );
 }
 
-export default Register
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired
+}
+
+export default connect(null,{setAlert})(Register);
