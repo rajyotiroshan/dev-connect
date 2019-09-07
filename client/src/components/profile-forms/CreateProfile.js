777b987b6be575a6f2269a408c45c5c448207fa9
rajan <rajangreedge@gmail.com>
2019-09-07 18:27:55 +0530

    CreateProfile cmpnt

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState} from 'react'
-import { withRouter} from 'react-router-dom';
+import { withRouter, Link} from 'react-router-dom';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import {createProfile} from '../../actions/profile';
@@ -209,9 +209,9 @@ const CreateProfile = ({ createProfile, history }) => {
           )}
 
           <input type='submit' className='btn btn-primary my-1' />
-          <a className='btn btn-light my-1' href='dashboard.html'>
+          <Link className='btn btn-light my-1' to='/dashboard'>
             Go Back
-          </a>
+          </Link>
         </form>
       </section>
     </Fragment>
