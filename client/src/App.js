4261ada782107c70b22efd0eb30d5392eca959ae
rajan <rajangreedge@gmail.com>
2019-09-08 15:01:34 +0530

    Profiles, ProfilesItem cmpnt

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
-
+import Profiles from './components/profiles/Profiles';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -39,6 +39,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={ Profiles}/>
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute
                 exact
