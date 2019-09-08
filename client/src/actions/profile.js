90a9fb1de64ffcf5038802b886df4bcc1c43c309
rajan <rajangreedge@gmail.com>
2019-09-08 12:42:32 +0530

    getProflies, getProfileById, getGithubRepos action creators

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,8 @@ import { setAlert } from './alert';
 
 import {
   GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
   UPDATE_PROFILE,
   PROFILE_ERROR,
   ACCOUNT_DELETED,
@@ -31,6 +33,70 @@ export const getCurrentProfile = () => async dispatch=>{
   }
 }
 
+//get all profiles
+export const getProfiles = () => async dispatch=>{
+  try {
+
+    //clear current user profile
+    dispatch({ type: CLEAR_PROFILE});
+    const res = await axios.get('/api/profile');
+    //console.log(res.data);
+    dispatch({
+      type: GET_PROFILES,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    })    
+  }
+}
+
+//Get profile by user_id
+
+export const getProfileById = (userId) => async dispatch=>{
+  try {
+    const res = await axios.get(`/api/profile/user/${userId}`);
+    //console.log(res.data);
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    })    
+  }
+}
+
+//Get github repos
+
+export const getGithubRepos = (username) => async dispatch=>{
+  try {
+    const res = await axios.get(`/api/profile/github/${username}`);
+    //console.log(res.data);
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    })
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: err.response.statusText,
+        status: err.response.status
+      }
+    })    
+  }
+}
 //create or update profile
 
 export const createProfile = (formData, history, edit= false) => async dispatch =>{
