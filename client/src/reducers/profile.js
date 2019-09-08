c4af5fafd15b017943d4dbe79331744b0782f028
rajan <rajangreedge@gmail.com>
2019-09-08 12:43:11 +0530

    GET_PROFILES, GET_REPOS reducers case

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -3,7 +3,9 @@ import {
   GET_PROFILE,
   PROFILE_ERROR,
   CLEAR_PROFILE,
-  UPDATE_PROFILE
+  UPDATE_PROFILE,
+  GET_PROFILES,
+  GET_REPOS
 } from '../actions/Types';
 
 const initialState = {
@@ -27,6 +29,12 @@ export default function(state=initialState, action){
         profile: payload,
         loading: false
       };
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      }
     case PROFILE_ERROR:
       return {
         ...state,
@@ -38,6 +46,12 @@ export default function(state=initialState, action){
         ...state,
         profile: null,
         repos: [],
+        loading: false
+      }
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       }
     default: return state;
