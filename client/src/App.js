c5aba7bccac45432fb5394463df6cef857256eb4
rajan <rajangreedge@gmail.com>
2019-09-08 08:23:51 +0530

    /add-experience private route

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
@@ -10,43 +10,55 @@ import './App.css';
 //Redux
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
-import {loadUser} from './actions/auth';
+import { loadUser } from './actions/auth';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 
-if(localStorage.token){
+if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
-const  App = ()=>{
-  
-  useEffect(()=>{
+const App = () => {
+  useEffect(() => {
     store.dispatch(loadUser());
-  }, []);//run once hence passed [] as 2nd arg.
+  }, []); //run once hence passed [] as 2nd arg.
 
   return (
-    <Provider store= { store }>
-          <Router>
-      <Fragment>
-        <Navbar/>
-        <Route exact path="/" component= { Landing } />
-        <section className="container">{/** for register and login page */}
-          <Alert/>
-          <Switch>
-            <Route exact path="/register" component={Register}/>
-            <Route exact path="/login" component={Login}/>
-            <PrivateRoute exact path="/dashboard" component={Dashboard}/>
-            <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-            <PrivateRoute exact path="/edit-profile" component={EditProfile}/>
-          </Switch>
-        </section>
-      </Fragment>   
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            {/** for register and login page */}
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/add-experience'
+                component={AddExperience}
+              />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
     </Provider>
-
-
-  )
-}
+  );
+};
 
 export default App;
