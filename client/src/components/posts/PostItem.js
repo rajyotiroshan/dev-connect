15a86009e0038107f5302705e0a473d909be8a58
rajan <rajangreedge@gmail.com>
2019-09-10 19:49:56 +0530

    fixed error: link to profile from post

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,4 +1,4 @@
-import React,{Fragment} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {Link} from 'react-router-dom';
 import Moment from 'react-moment';
@@ -18,7 +18,7 @@ const PostItem = ({ addLike, removeLike,  deletePost , auth, post:{
   return (
     <div className='post bg-white p-1 my-1'>
       <div>
-        <Link to='/profile'>
+        <Link to={`/profile/${user}`}>
           <img className='round-img' src={avatar} alt='' />
           <h4>{name}</h4>
         </Link>
