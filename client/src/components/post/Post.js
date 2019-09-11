ca3df305e67cb55bcb7f93bd4b5f2b3e767a3ae7
rajan <rajangreedge@gmail.com>
2019-09-11 12:21:08 +0530

    render comments for a post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -6,6 +6,8 @@ import Spinner from "../layouts/Spinner";
 import PostItem from '../posts/PostItem';
 import {getPost} from '../../actions/post';
 import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
+
 
 const Post = ({getPost, post:{post, loading}, match})=> {
   
@@ -17,6 +19,11 @@ const Post = ({getPost, post:{post, loading}, match})=> {
       <Link to='/posts' className='btn'>Back to Posts</Link>
       <PostItem post={post} showActions={false}/>
       <CommentForm post_id={post._id}/>
+      <div className="comments">
+        { post.comments.map(comment=>(
+          <CommentItem key={comment._id} comment={comment} post_id={post._id}/>
+        ))}
+      </div>
     </Fragment>
   )
 }
